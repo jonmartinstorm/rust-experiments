a5e6334c3e24c410275f208524510af812db1ef8
Jon-Martin Storm <jonmartin.storm@gmail.com>
2021-01-21 14:53:25 +0100

    working example of echo unix server

--- unix-async/src/main.rs
+++ unix-async/src/main.rs
@@ -1,14 +1,25 @@
 use tokio::net::UnixListener;
+use std::fs;
+use tokio::io::copy;
 
 #[tokio::main]
 async fn main() {
-    let listener = UnixListener::bind("/path/to/the/socket").unwrap();
+    let path = "/tmp/test.sock";
+    fs::remove_file(path).unwrap();
+    let listener = UnixListener::bind(path).unwrap();
+    println!("Listen on {}", path);
     loop {
         match listener.accept().await {
-            Ok((stream, _addr)) => {
+            Ok((mut stream, _addr)) => {
                 println!("new client!");
+                tokio::spawn(async move {
+                    let (mut reader, mut writer) = stream.split();
+                    copy(&mut reader, &mut writer).await.unwrap();
+                    println!("Client left!");
+                });
             }
-            Err(e) => { /* connection failed */ }
+            Err(e) => { println!("Err: {}", e) }
         }
     }
+    
 }
