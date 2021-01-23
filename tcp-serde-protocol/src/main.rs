8edeb8aad047d7b43d771c04a0ffb5237d97d9e8
Jon-Martin Storm <jonmartin.storm@gmail.com>
2021-01-23 21:30:16 +0100

    it works! WOW

--- tcp-serde-protocol/src/main.rs
+++ tcp-serde-protocol/src/main.rs
@@ -1,3 +1,67 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::{TcpListener};
+// use tokio::io::copy;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Header {
+    len: i32,
+    msg_type: String,
+}
+
+
+#[tokio::main]
+async fn main() {
+    let tcplistener = TcpListener::bind("127.0.0.1:9977").await.unwrap();
+    println!("Listen on port: 9977");
+
+    let t = tokio::spawn(async move {
+        listen_tcp(tcplistener).await;
+    });
+
+    t.await.unwrap();
+}
+
+async fn listen_tcp(listener: TcpListener) {
+    loop {
+        match listener.accept().await {
+            Ok((mut stream, _addr)) => {
+                println!("new TCP client! {:?}", _addr);
+                tokio::spawn(async move {
+                    let (mut reader, mut writer) = stream.split();
+                    // read header length
+                    let mut len = vec![0; 1];
+                    reader.read(&mut len).await.unwrap();
+                    println!("Header len {}", &len[0]);
+
+                    // read header
+                    let mut header = vec![0; len[0] as usize];
+                    reader.read(&mut header).await.unwrap();
+                    let header_string = std::str::from_utf8(&header).unwrap();
+                    println!("Header string {}", header_string);
+                    let header: Header = serde_json::from_str(header_string).unwrap();
+                    println!("Header {:?}", header);
+
+                    // read payload
+                    let mut payload = vec![0; header.len as usize];
+                    reader.read(&mut payload).await.unwrap();
+                    let payload_string = std::str::from_utf8(&payload).unwrap();
+                    println!("Payload string {}", payload_string);
+                    let payload: Point = serde_json::from_str(payload_string).unwrap();
+                    println!("Payload {:?}", payload);
+
+                    // write something
+                    writer.write_all(b"hello world!\n").await.unwrap();
+                    println!("TCP Client left!");
+                });
+            }
+            Err(e) => { println!("Err: {}", e) }
+        }
+    }
 }
