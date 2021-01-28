53fa0bf4cce1c674603513124e12300754cfeec8
Jon-Martin Storm <jonmartin.storm@gmail.com>
2021-01-28 15:11:10 +0100

    unix version

---
+++ modbus-test2/python-client/modbus-gateway.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+"""
+Pymodbus Server With Updating Thread
+--------------------------------------------------------------------------
+
+This is an example of having a background thread updating the
+context while the server is operating. This can also be done with
+a python thread::
+
+    from threading import Thread
+
+    thread = Thread(target=updating_writer, args=(context,))
+    thread.start()
+"""
+# --------------------------------------------------------------------------- #
+# import the modbus libraries we need
+# --------------------------------------------------------------------------- #
+from pymodbus.server.asynchronous import StartTcpServer
+from pymodbus.device import ModbusDeviceIdentification
+from pymodbus.datastore import ModbusSequentialDataBlock
+from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
+from pymodbus.transaction import ModbusRtuFramer, ModbusAsciiFramer
+
+# --------------------------------------------------------------------------- #
+# import the twisted libraries we need
+# --------------------------------------------------------------------------- #
+from twisted.internet.task import LoopingCall
+
+# --------------------------------------------------------------------------- #
+# import the random libraries we need
+# --------------------------------------------------------------------------- #
+import random
+import socket
+import json
+
+
+# --------------------------------------------------------------------------- #
+# configure the service logging
+# --------------------------------------------------------------------------- #
+import logging
+logging.basicConfig()
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+# --------------------------------------------------------------------------- #
+# define your callback process
+# --------------------------------------------------------------------------- #
+
+
+def updating_writer(a):
+    """ A worker process that runs every so often and
+    updates live values of the context. It should be noted
+    that there is a race condition for the update.
+
+    :param arguments: The input arguments to the call
+    """
+    log.debug("updating the context")
+    context = a[0]
+    register = 4
+    slave_id = 0x00
+    address = 0x01
+
+    ## read some values
+    read_values = context[slave_id].getValues(3, 0, count=2)
+    log.debug(f"{read_values}")
+
+    # Update the values some way or another, for example from another server
+   
+    # find out how to put this into another file
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    path = "/tmp/test.sock"
+    sock.connect(path)
+
+    payload = {
+        "x": read_values[0], 
+        "y": read_values[1],  
+    }
+    payload_encoded = json.dumps(payload, separators=(',', ':'))    
+    header = {
+        "len": len(payload_encoded),
+        "msg_type": "get_info",
+    }
+    header_encoded = json.dumps(header, separators=(',', ':'))
+    header_len = len(header_encoded)
+
+    sock.send(bytes([header_len]))
+    sock.send(bytes(header_encoded, 'utf-8'))
+    sock.send(bytes(payload_encoded, 'utf-8'))
+
+    response_data = sock.recv(1024)
+
+    response_data = json.loads(response_data.decode('utf-8').strip())
+
+    for key in response_data:
+        log.debug(f"{key}: {response_data[key]}")
+
+    sock.close()
+    values = [1,2,3,4,5]
+    #log.debug(values)
+    values = [random.randint(1, 5) for v in values]
+    values = [response_data["value"]]
+    log.debug("new values: " + str(values))
+    context[slave_id].setValues(register, address, values)
+
+
+def run_updating_server():
+    # ----------------------------------------------------------------------- # 
+    # initialize your data store
+    # ----------------------------------------------------------------------- # 
+    
+    store = ModbusSlaveContext(
+        di=ModbusSequentialDataBlock(0, [16]*100),
+        co=ModbusSequentialDataBlock(0, [17]*100),
+        hr=ModbusSequentialDataBlock(0, [18]*100),
+        ir=ModbusSequentialDataBlock(0, [19]*100))
+    context = ModbusServerContext(slaves=store, single=True)
+    # log.debug(context[0])
+    
+    # ----------------------------------------------------------------------- # 
+    # initialize the server information
+    # ----------------------------------------------------------------------- # 
+    identity = ModbusDeviceIdentification()
+    identity.VendorName = 'pymodbus'
+    identity.ProductCode = 'PM'
+    identity.VendorUrl = 'http://github.com/bashwork/pymodbus/'
+    identity.ProductName = 'pymodbus Server'
+    identity.ModelName = 'pymodbus Server'
+    identity.MajorMinorRevision = '2.3.0'
+    
+    # ----------------------------------------------------------------------- # 
+    # run the server you want
+    # ----------------------------------------------------------------------- # 
+    time = 1  # 1 seconds delay
+    loop = LoopingCall(f=updating_writer, a=(context,))
+    loop.start(time, now=False) # initially delay by time
+    StartTcpServer(context, identity=identity, address=("localhost", 5020))
+
+
+if __name__ == "__main__":
+    run_updating_server()
